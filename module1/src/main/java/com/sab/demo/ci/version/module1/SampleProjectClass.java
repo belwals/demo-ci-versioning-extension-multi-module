e219f317d5cce19bb18402af1a2338e0590d8d5e
Saurabh Belwal <saurabh.belwal01@gmail.com>
2021-09-04 00:41:40 +0530

    Initial commit with functional changes

---
+++ module1/src/main/java/com/sab/demo/ci/version/module1/SampleProjectClass.java
@@ -0,0 +1,7 @@
+package com.sab.demo.ci.version.module1;
+
+public class SampleProjectClass {
+    public SampleProjectClass() {
+        System.out.println("Constructor called!");
+    }
+}
---
+++ module2/src/main/java/com/sab/demo/ci/version/module2/SampleProjectClass.java
@@ -0,0 +1,7 @@
+package com.sab.demo.ci.version.module2;
+
+public class SampleProjectClass {
+    public SampleProjectClass() {
+        System.out.println("Constructor called!");
+    }
+}
